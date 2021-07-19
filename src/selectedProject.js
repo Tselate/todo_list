07940a123843fd6ab36ddadd3175c8ca191986b0
Tselate <Tselatedawit@outlook.com>
2021-07-18 18:08:23 -0600

    New task display edited

--- src/selectedProject.js
+++ src/selectedProject.js
@@ -48,47 +48,49 @@ function newTaskCancelled () {
     newTask.style.visibility = "visible" 
 }
 
+//Save new array to local storage 
 let newTaskName = JSON.parse(localStorage.getItem("taskArrayKey")) || []
+
+projectList.addEventListener("click", function addTaskandMakeArray (e) {
+    let idOfProjectElement = e.target.id
+    let selectedProject = document.getElementById(idOfProjectElement)
+    taskDiv.innerHTML = "Project: " + selectedProject.innerHTML 
+    newTask.style.visibility = "visible"
+
+   
+
+    addNewTask.addEventListener("click", function printOut (e) {
+        e.preventDefault()
+        tNameId = selectedProject.innerHTML
+        const toDo = new task (tTitle.value, tDescription.value, tDueDate.value, tPriorty.value, tNotes.value, tNameId)
+        newTaskName.push(toDo)
+        localStorage.setItem("taskArrayKey", JSON.stringify(newTaskName))
+    })  
+   
+    let taskList = newTaskName.filter( function displayIt (list) {
+        return list.taskNameId === selectedProject.innerHTML
+    })
+
+
+
+    console.log(taskList)
+
+   // localStorage.clear("taskArrayKey")
+    
+    // console.log(newTaskName)
+    // const toDo = new task 
+    // for (let i = 0; i < newTaskName.length; i++) {
+    //     if (newTaskName[i].taskNameId === selectedProject.innerHTML) {
+    //        console.log(newTaskName[i].taskNameId)
+        
+    //     }
+    // }
+})
 
 
 export function slectedProjectExported () {
-    
-    projectList.addEventListener("click", function addTaskandMakeArray (e) {
-        let idOfProjectElement = e.target.id
-        let selectedProject = document.getElementById(idOfProjectElement)
-        taskDiv.innerHTML = "Project: " + selectedProject.innerHTML 
-        newTask.style.visibility = "visible"
-
-       
-
-        addNewTask.addEventListener("click", function printOut (e) {
-            e.preventDefault()
-            tNameId = selectedProject.innerHTML
-            const toDo = new task (tTitle.value, tDescription.value, tDueDate.value, tPriorty.value, tNotes.value, tNameId)
-            console.log(toDo.taskTitile)
-            console.log(toDo.taskDescription) 
-            console.log(toDo.taskDueDate)
-            console.log(toDo.taskPriorty) 
-            console.log(toDo.taskNotes)
-        
-            newTaskName.push(toDo)
-            localStorage.setItem("taskArrayKey", JSON.stringify(newTaskName))
-            console.log(newTaskName)
-
-    
-        })  
-
-        const toDo = new task
-        for (let i = 0; i < newTaskName.length; i++) {
-            if (toDo.taskNameId = selectedProject.innerHTML) {
-                taskDiv.innerHTML = newTaskName[toDo.taskTitile]
-            }
-        }
-
-        newTask.addEventListener("click", createNewTask)
-        //displayData(selectedProject.innerHTML)
-    })
-  
+    newTask.addEventListener("click", createNewTask)
+    //displayData(selectedProject.innerHTML)
 }
 
 
